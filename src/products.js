8c8e0b09fe2054cf3fc1fb916411879bdc628d28
Sachitkothari <61432064+Sachitkothari@users.noreply.github.com>
2024-01-10 10:14:17 +0400

    Initial commit

---
+++ src/products.js
@@ -0,0 +1,66 @@
+import DominosShirt from "./assets/products/DominosShirt.webp";
+import AMDGPU from "./assets/products/PG6800XT.png";
+import Protein from "./assets/products/CliffBars.jpg";
+import Ring from "./assets/products/TheOneRing.png";
+import AMDCPU from "./assets/products/Ryzen5800X.jpg";
+import Keyboard from "./assets/products/YamahaKeyboard.webp";
+import Paper from "./assets/products/OrigamiPaper.jpg";
+import Arcana from "./assets/products/QOPArcana.jpg";
+import Zhongli from "./assets/products/ZhongliCosplay.jpg";
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Dominos Shirt",
+        price: 10.0,
+        productImage: DominosShirt,
+    },
+    {
+        id: 2,
+        productName: "AMD 6800XT GPU",
+        price: 499.99,
+        productImage: AMDGPU,
+    },
+    {
+        id: 3,
+        productName: "Cliff Protein Bars",
+        price: 17.46,
+        productImage: Protein,
+    },
+    {
+        id: 4,
+        productName: "The one ring to rule them all",
+        price: 11.2,
+        productImage: Ring,
+    },
+    {
+        id: 5,
+        productName: "Zhongli Costume",
+        price: 108.81,
+        productImage: Zhongli,
+    },
+    {
+        id: 6,
+        productName: "Yamaha PSR-S650",
+        price: 999.0,
+        productImage: Keyboard,
+    },
+    {
+        id: 7,
+        productName: "Origami Paper",
+        price: 11.55,
+        productImage: Paper,
+    },
+    {
+        id: 8,
+        productName: "dotA2 QOP Arcana",
+        price: 245.0,
+        productImage: Arcana,
+    },
+    {
+        id: 9,
+        productName: "Ryzen 7 5800X CPU",
+        price: 239.0,
+        productImage: AMDCPU,
+    },
+];
